56d66965f81441deaf77cb1216ebb75a69d91085
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-10-18 15:02:36 -0500

    persist-messages

--- server/index.js
+++ server/index.js
@@ -4,20 +4,62 @@ import logger from 'morgan'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 
+import { client } from './database.js'
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
 
 const server = createServer(app)
 
-const io = new Server(server)
+const io = new Server(server, {
+  connectionStateRecovery: {}
+})
 
-io.on('connection', (socket) => {
+try {
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id SERIAL PRIMARY KEY,
+        content TEXT
+    )`)
+} catch (e) {
+  console.error(e)
+}
+
+io.on('connection', async (socket) => {
   console.log('un usuario se ha conectado')
 
   socket.on('disconnect', () => {
     console.log('un usuario se ha desconectado')
   })
+
+  socket.on('chat message', async (msg) => {
+    let result
+    try {
+      result = await client.query(
+        'INSERT INTO messages (content) VALUES ($1) RETURNING id',
+        [msg]
+      )
+    } catch (e) {
+      console.error(e)
+      return
+    }
+    io.emit('chat message', msg, result.rows[0].id)
+  })
+  if (!socket.recovered) {
+    try {
+      const results = await client.query(
+        'SELECT * FROM messages WHERE id > $1',
+        [socket.handshake.auth.serverOffSet ?? 0]
+      )
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id)
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
 })
 
 app.use(logger('dev'))
